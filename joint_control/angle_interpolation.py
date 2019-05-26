cc85f6b80eac30c5553c27e08c19ebd1156c5adc
kaiser.mj <marius.j.kaiser@campus.tu-berlin.de>
2019-05-26 13:35:38 +0200

    fist version

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -21,7 +21,7 @@
 
 
 from pid import PIDAgent
-from keyframes import hello
+from keyframes import hello, leftBackToStand, leftBellyToStand, rightBackToStand, wipe_forehead, rightBellyToStand
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -32,6 +32,8 @@ class AngleInterpolationAgent(PIDAgent):
                  sync_mode=True):
         super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.keyframes = ([], [], [])
+        self.start_time = None # starttime
+        self.spline = [] # spline
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
@@ -41,10 +43,48 @@ class AngleInterpolationAgent(PIDAgent):
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
-
+        names, times, keys = keyframes
+        if self.start_time == None:
+            self.start_time = perception.time
+        if self.spline == []:
+            names, times, keys = keyframes
+            for joint_i,name in enumerate(names):
+                self.spline.append([])
+                for frame in range(len(times[joint_i])-1):
+                    self.spline[joint_i].append([])
+                    P_i0 = (p0_x,p0_y) = (times[joint_i][frame], keys[joint_i][frame][0])
+                    P_i1 = (p1_x,p1_y) = (times[joint_i][frame] + keys[joint_i][frame][1][1], keys[joint_i][frame][0] + keys[joint_i][frame][1][2])
+                    P_i2 = (p2_x,p2_y) = (times[joint_i][frame] + keys[joint_i][frame][2][1], keys[joint_i][frame][0] + keys[joint_i][frame][2][2])
+                    P_i3 = (p3_x,p3_y) = (times[joint_i][frame+1], keys[joint_i][frame+1][0])
+                    t_0 = times[joint_i][frame]
+                    t_4 = times[joint_i][frame+1]
+                    self.spline[joint_i][frame].append(t_0)
+                    self.spline[joint_i][frame].append(t_4)
+                    b = bezier((p0_x,p0_y),(p1_x,p1_y),(p2_x,p2_y),(p3_x,p3_y))
+                    self.spline[joint_i][frame].append(b)    
+        else:
+            current_time = round(perception.time - self.start_time, 2)
+            for i,joint in enumerate(names):
+                for t in range(len(self.spline[i])):
+                    t_min =  self.spline[i][t][0]
+                    t_max =  self.spline[i][t][1]
+                    if t_min <= current_time and current_time <= t_max:
+                        target_joints[joint] =  self.spline[i][t][2][1](current_time)
+                        break
         return target_joints
 
+def bezier((p0_x,p0_y),(p1_x,p1_y),(p2_x,p2_y),(p3_x,p3_y)):
+    # i = ((t - p0_x) / (p0_x - p3_x))
+    def b_x(t):
+        return pow((1-((t - p0_x) / (p0_x - p3_x))),3)*p0_x+3*pow((1-((t - p0_x) / (p0_x - p3_x))),2)*((t - p0_x) / (p0_x - p3_x))*p1_x+3*(1-((t - p0_x) / (p0_x - p3_x)))*pow(((t - p0_x) / (p0_x - p3_x)),2)*p2_x+pow(((t - p0_x) / (p0_x - p3_x)),3)*p3_x
+    def b_y(t):
+        return pow((1-((t - p0_x) / (p0_x - p3_x))),3)*p0_y+3*pow((1-((t - p0_x) / (p0_x - p3_x))),2)*((t - p0_x) / (p0_x - p3_x))*p1_y+3*(1-((t - p0_x) / (p0_x - p3_x)))*pow(((t - p0_x) / (p0_x - p3_x)),2)*p2_y+pow(((t - p0_x) / (p0_x - p3_x)),3)*p3_y
+    x = b_x
+    y = b_y
+    return (x,y)
+        
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    #agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = leftBackToStand()
     agent.run()
--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 1
+        self.Ki = 0.01
+        self.Kd = 0.01
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,7 +53,10 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
-
+        e0 = target - sensor
+        self.u = self.u + (self.Kp + self.Ki*self.dt+(self.Kd/self.dt))*e0 - (self.Kp+(2*self.Kd)/self.dt)*self.e1 + (self.Kd/self.dt)*self.e2
+        self.e2 = self.e1
+        self.e1 = e0
         return self.u
 
 
